984cdfa545ca7078a079fc1df9881c78aa616bde
Namachivayam2001 <namachivayam10419u25030@gmail.com>
2023-08-24 07:15:11 +0530

    userid validation added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const form = document.getElementById("form");
 const uid = document.getElementById("userid");
+const uidError = document.getElementById("uidError");
 const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
 const male = document.getElementById("male");
@@ -7,6 +9,11 @@ const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const pass = document.getElementById("password");
 const confirm = document.getElementById("confirmPassword");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    validate();
+})
 
 const validate = () => {
     if(uidValidate()){
@@ -29,14 +36,21 @@ const validate = () => {
 }
 
 const uidValidate = () => {
-    const uidRegex = /^[a-zA-Z0-9]{6,}$/;
-    if(uidRegex.test(uid)){
-        fname.focus();
-        return true;
-    } else{
-        document.getElementById("useridError").innerHTML = "Use above 5 char spcial char not allowed";
+    const uidRegex = /[a-zA-Z0-9]w+/g;
+    const uidval = uid.value.trim();
+    if(uidval == ""){
+        uidError.innerHTML = "Userid not be empty";
         uid.focus();
         return false;
-
+    } else if(!uidRegex.test(uidval)){
+        uidError.innerHTML = "No special characters allow";
+        uid.focus();
+        return false;
+    } else if(uidval.length <= 5){
+        uidError.innerHTML = "Userid graterthen 5 char"
+    } else{
+        uidError.innerHTML = "";
+        fname.focus();
+        return false;
     }
 }
